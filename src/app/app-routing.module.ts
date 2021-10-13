3979e08ed7bf47cb9ac2269835e8e7ed8fdcdd97
Lara Souza <laragabriellysouzabatista@gmail.com>
2021-10-12 23:46:22 -0300

    changes on text-img and created routing-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BerriesComponent } from './berries/berries.component';
+import { RoutingGuard } from './guards/routing.guard';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {
   path:"",
   component: NavBarComponent,
+  canActivate:[RoutingGuard],
   children:[
     {
       path : "home",
---
+++ src/app/guards/routing.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoutingGuard implements CanActivate {
+  constructor(
+    private router:Router
+  ){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(state.url == "/"){
+      this.router.navigateByUrl("/home")
+      }
+      return true;
+  }
+  
+}
